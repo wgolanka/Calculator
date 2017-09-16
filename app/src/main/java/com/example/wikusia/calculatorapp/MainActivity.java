dfc6e7240393d031226b165099cffb626858dd7c
Wiktoria Golanka <s15565@pjwstk.edu.pl>
2017-09-16 11:34:31 +0200

    Made theme style changes.

--- app/src/main/java/com/example/wikusia/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/wikusia/calculatorapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wikusia.calculatorapp;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -29,6 +30,10 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.hide();
+
         if (savedInstanceState != null)
         {
             operand = savedInstanceState.getDouble("OPERAND");
